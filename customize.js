623ba4e4f7f5e3ac0a8385be3848970bcb45e751
Johan <johanp877@gmail.com>
2022-03-22 01:00:23 -0500

    Actualización de las funciones de la tarjeta SITP

--- customize.js
+++ customize.js
@@ -1,9 +1,11 @@
-const tarjeta = document.querySelector('#tarjeta')
-	  btnAbrirFormulario = document.querySelector('#btn-abrir-formulario')
-	  formulario = document.querySelector('#formulario-tarjeta')
-	  numeroTarjeta = document.querySelector('#tarjeta .numero')
-	  nombreTarjeta = document.querySelector('#tarjeta .nombre')
+const tarjeta = document.querySelector('#tarjeta'),
+	  btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+	  formulario = document.querySelector('#formulario-tarjeta'),
+	  numeroTarjeta = document.querySelector('#tarjeta .numero'),
+	  nombreTarjeta = document.querySelector('#tarjeta .nombre'),
+	  nombreCedula = document.querySelector('#tarjeta .cedula')
 
+	  
 
 // * Volteamos la tarjeta para mostrar el frente.
 const mostrarFrente = () => {
@@ -22,4 +24,71 @@ btnAbrirFormulario.addEventListener('click', () => {
 	btnAbrirFormulario.classList.toggle('active');
 	formulario.classList.toggle('active');
 });
+
+
+// * Input numero de tarjeta
+formulario.inputNumero.addEventListener('keyup', (e) => {
+
+	if(!tarjeta.classList.contains('active')){
+		tarjeta.classList.toggle('active');
+	}
+
+	let valorInput = e.target.value;
+
+	formulario.inputNumero.value = valorInput
+	// Eliminamos espacios en blanco
+	.replace(/\s/g, '')
+	// Eliminar las letras
+	.replace(/\D/g, '')
+	// Ponemos espacio cada cuatro numeros
+	.replace(/([0-9]{4})/g, '$1 ')
+	// Elimina el ultimo espaciado
+	.trim();
+
+	numeroTarjeta.textContent = valorInput;
+
+	if(valorInput == ''){
+		numeroTarjeta.textContent = '#### #### #### ####';
+	}
+
+	// Volteamos la tarjeta para que el usuario vea el frente.
+
+});
+
+
+// * Input nombre de tarjeta
+formulario.inputNombre.addEventListener('keyup', (e) => {
+	let valorInput = e.target.value;
+
+	formulario.inputNombre.value = valorInput.replace(/[0-9]/g, '');
+	nombreTarjeta.textContent = valorInput;
+
+	if(valorInput == ''){
+		nombreTarjeta.textContent = 'Jhon Doe';
+	}
+
+	mostrarFrente();
+});
+
+formulario.inputCedula.addEventListener('keyup', (e) => {
+	let valorInput = e.target.value;
+
+	formulario.inputCedula.value = valorInput
+		// Eliminamos espacios en blanco
+		.replace(/\s/g, '')
+		// Eliminar las letras
+		.replace(/\D/g, '')
+		// Elimina el ultimo espaciado
+		.trim();
+	
+	nombreCedula.textContent = valorInput;
+
+	if(valorInput == ''){
+		nombreCedula.textContent = '#########';
+	}
+
+	mostrarFrente();
+});
+
+
 
