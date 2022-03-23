0b9e56c10aca67583da634d996cd7200dd6663f4
lariza azuaje <azuajelariza@gmail.com>
2022-03-22 21:38:55 -0600

    formulario de registro listo by Lariza

---
+++ recarga.js
@@ -0,0 +1,11 @@
+let botonRecarga = document.getElementById('btn');
+
+Swal.fire({
+    title: '¡Tu recarga fue realizada con exito!',
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
